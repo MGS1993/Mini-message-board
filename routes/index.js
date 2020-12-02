72c6073b35fbca7b626364076f4e3d6465d26dc3
Manuel Guzman <m_guzman74@yahoo.com>
2020-12-02 04:33:16 -0600

    minor css added

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var format = require('date-fns/format')
 
 const messages = [
   {
     text: 'Hi there!',
     user: "Amando",
-    added: new Date()
+    added: format(new Date(), 'EEE h:mm')
   },
   {
     text: "Hello, World!",
     user: "Charles",
-    added: new Date()
+    added: format(new Date(), 'EEE h:mm')
   }
 ];
 /* GET home page. */
@@ -18,7 +19,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini MessageBoard', messages: messages });
 });
 router.post('/new', function (req, res) {
-  messages.push({text: req.body.message, user: req.body.name, added: new Date()});
+  messages.push({text: req.body.message, user: req.body.name, added: format(new Date(), 'EEE h:mm')});
   res.redirect('/')
 })
 
